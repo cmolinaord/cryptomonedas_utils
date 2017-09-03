9ce103acd835abdd402020c7123bd645a53d69d7
cmolinaord <carlosmolina.ord@gmail.com>
2017-09-03 17:07:06 +0200

    Adding colored increment/decrement last hour history price

--- get_price.sh
+++ get_price.sh
@@ -3,6 +3,17 @@
 WEB_ROOT="https://api.coinmarketcap.com/v1/ticker"
 COIN=$1
 
+if [[ $2 == '--no-color' ]]
+then
+	unset RED
+	unset GREEN
+	unset NC
+else
+	RED='\033[1;31m'
+	GREEN='\033[1;32m'
+	NC='\033[0m'
+fi
+
 if [[ $1 == "" ]]
 then
 	echo "Dime una moneda por favor"
@@ -10,5 +21,11 @@ else
 	content=$(curl -s "$WEB_ROOT/$COIN/")
 	symbol=$(echo $content | sed "s/,/\n/g" | grep "symbol" | cut -d'"' -f4)
 	price_usd=$(echo $content | sed "s/,/\n/g" | grep "price_usd" | cut -d'"' -f4)
-	echo "1 $symbol = $price_usd USD"
+	change_1h=$(echo $content | sed "s/,/\n/g" | grep "percent_change_1h" | cut -d'"' -f4)
+	if [[ $change_1h == "-*" ]]
+	then
+		echo -e "1$symbol = $price_usd USD (1h: $RED$change_1h%$NC)"
+	else
+		echo -e "1$symbol = $price_usd USD (1h: $GREEN$change_1h%$NC)"
+	fi
 fi
