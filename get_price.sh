01bfe532ddb14958ada6009a454b5fa634c99fbd
cmolinaord <carlosmolina.ord@gmail.com>
2017-09-06 18:12:52 +0200

    Added option to retrieve 1h, 24h or 7d period of change

--- get_price.sh
+++ get_price.sh
@@ -2,8 +2,19 @@
 
 WEB_ROOT="https://api.coinmarketcap.com/v1/ticker"
 COIN=$1
+TIME=$2
+if [[ $TIME == "1h" || $TIME == "24h" || $TIME == "7d" ]]
+then
+	CHANGE_STRING=`echo "percent_change_$TIME"`
+elif [[ $TIME == "" ]]
+then
+	CHANGE_STRING=""
+else
+	echo "   ***Error: TIME must be '1h', '24h' or '7d'"
+	exit
+fi
 
-if [[ $2 == '--no-color' ]]
+if [[ $3 == '--no-color' ]]
 then
 	unset RED
 	unset GREEN
@@ -14,18 +25,28 @@ else
 	NC='\033[0m'
 fi
 
-if [[ $1 == "" ]]
+if [[ $COIN == "" ]]
 then
-	echo "Dime una moneda por favor"
+	echo "   ***Error: Dime una moneda por favor"
 else
 	content=$(curl -s "$WEB_ROOT/$COIN/")
 	symbol=$(echo $content | sed "s/,/\n/g" | grep "symbol" | cut -d'"' -f4)
 	price_usd=$(echo $content | sed "s/,/\n/g" | grep "price_usd" | cut -d'"' -f4)
-	change_1h=$(echo $content | sed "s/,/\n/g" | grep "percent_change_1h" | cut -d'"' -f4)
-	if [[ $change_1h == "-*" ]]
+	if [[ $CHANGE_STRING != "" ]]
 	then
-		echo -e "1$symbol = $price_usd USD (1h: $RED$change_1h%$NC)"
+		change=$(echo $content | sed "s/,/\n/g" | grep $CHANGE_STRING | cut -d'"' -f4)
+		if [[ $CHANGE == "-*" ]]
+		then
+			echo -e "1$symbol = $price_usd USD ($TIME: $RED$change%$NC)"
+		else
+			echo -e "1$symbol = $price_usd USD ($TIME: $GREEN$change%$NC)"
+		fi
 	else
-		echo -e "1$symbol = $price_usd USD (1h: $GREEN$change_1h%$NC)"
+		if [[ $CHANGE == "-*" ]]
+		then
+			echo -e "1$symbol = $price_usd USD"
+		else
+			echo -e "1$symbol = $price_usd USD"
+		fi
 	fi
 fi
